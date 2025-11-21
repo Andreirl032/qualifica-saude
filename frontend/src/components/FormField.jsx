ab5a1f1904a560401b5d2aabd669bbe2a6ec4509
Andreirl032 <andrei.ramos.lopes@gmail.com>
2025-11-21 18:57:30 -0300

    consertado estilo e add imagem de medico

--- frontend/src/components/FormField.jsx
+++ frontend/src/components/FormField.jsx
@@ -1,8 +1,8 @@
 export function FormField({ label, error, children }) {
   return (
-    <label className="form-control">
+    <label className="form-control flex flex-col justify-start gap-1">
       <div className="label">
-        <span className="label-text">{label}</span>
+        <span className="label-text justify-start text-gray-700">{label}</span>
       </div>
       {children}
       {error && <span className="text-error text-sm">{error}</span>}
