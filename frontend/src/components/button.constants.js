af6f8c8594cba360c980c5c6726e05e4cab63cf9
Andreirl032 <andrei.ramos.lopes@gmail.com>
2026-01-20 19:29:39 -0300

    Reset project for Next.js rewrite

--- frontend/src/components/button.constants.js
+++
@@ -1,9 +0,0 @@
-export const buttonVariantClasses = {
-  primary: 'btn-primary',
-  secondary: 'btn-secondary',
-  outline: 'btn-outline',
-  link: 'btn-link',
-  ghost: 'btn-ghost',
-  success: 'btn-success',
-  error: 'btn-error',
-}
